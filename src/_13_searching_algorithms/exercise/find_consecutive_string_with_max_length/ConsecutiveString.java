51a53bbe832159ac4a9a305018ea63489d27bca5
unknown <you@example.com>
2022-05-22 23:49:19 +0700

    up

--- src/_13_searching_algorithms/exercise/find_consecutive_string_with_max_length/ConsecutiveString.java
+++ src/_13_searching_algorithms/exercise/find_consecutive_string_with_max_length/ConsecutiveString.java
@@ -6,27 +6,31 @@ import java.util.Scanner;
 public class ConsecutiveString {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        LinkedList<Character> max = new LinkedList<>();
+        LinkedList<Character> list = new LinkedList<>();
 
-        LinkedList<Character> maxString = new LinkedList<>();
-        LinkedList<Character> listString = new LinkedList<>();
-
-        System.out.print("Nhập chuỗi cần tìm: ");
+        System.out.print("Nhập chuỗi: ");
         String string = input.nextLine();
 
-        for (int i = 0; i < string.length(); i++) {
-            if (listString.size() > 1) {
-                if (string.charAt(i) <= listString.getLast() && listString.contains(string.charAt(i))) {
-                    listString.clear();
-                }
+        // Tìm chuỗi liên tiếp có độ dài lớn nhất
+        for (int i = 0; i < string.length(); i++) { 		//1 Vòng lặp
+            if (list.size() > 1 && string.charAt(i) <= list.getLast() &&
+                    list.contains(string.charAt(i))) {
+                list.clear(); //1 câu lệnh
             }
-            listString.add(string.charAt(i));
-            if (maxString.size() < listString.size()) {
-                maxString.clear();
-                maxString.addAll(listString);
+
+            list.add(string.charAt(i)); // 1 câu lệnh
+
+            if (list.size() > max.size()) { // 1 câu lệnh
+                max.clear();
+                max.addAll(list);
             }
         }
-        for (Character character : maxString) {
-            System.out.print(character);
+
+        // Hiển thị chuỗi liên tiếp dài nhất
+        for (Character ch: max) { // 1 vòng lặp
+            System.out.print(ch); // 1 câu lệnh
         }
+        System.out.println();
     }
 }
